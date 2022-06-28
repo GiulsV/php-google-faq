7ddb6531b44626ab9f4c3cd6ca65f0964290b045
GiuliaV <valente.giulia93@gmail.com>
2022-06-28 15:45:57 +0200

    Creato array

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il "Database" (in questo caso un semplice array) e la visualizzazione di queste domande e risposte con PHP. -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include 'data.php';
+
+     ?>
+    <title>Google F.A.Q.</title>
+</head>
+<body>
+
+</body>
+</html>
